27e9d905d064a7a8b4603f0d6a2de009d7497e9a
Duffy Gillman <duffy@rsmart.com>
2012-01-11 08:39:03 -0700

    ACAD-496 adjusted calls for autosuggest functionality to match changes in signature with 1.1 merge

--- devwidgets/sendmessage/javascript/sendmessage.js
+++ devwidgets/sendmessage/javascript/sendmessage.js
@@ -223,7 +223,7 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
 //                                        }
                                 }
                             });
-                            add(suggestions);
+                            add(suggestions, query);
                         }
                     }, searchoptions);
                 };
