f7bfd4229e54e9e75af352afb0f47ce3cc3e4218
Chris Roby <christopher.roby@gmail.com>
2012-01-28 13:25:44 -0800

    ACAD-657 Configure the widget lists

--- dev/configuration/config_custom.js
+++ dev/configuration/config_custom.js
@@ -23,6 +23,74 @@ define(["config/config"], function(config) {
     config.displayLanguage = true;
 
     /**
+     * Customize the widgets that can be placed in sakaidocs
+     *
+     * Order matters here, so you can re-arrange however you'd like these
+     * widgets to show up
+     */
+    config.enabledWidgets = [
+        "embedcontent",
+        "discussion",
+        "remotecontent",
+        "inlinecontent",
+        "sakai2samigo",
+        "sakai2calendar",
+        "googlemaps",
+        "comments",
+        "jisccontent",
+        "sakai2assignments",
+        "rss",
+        "basiclti",
+        "ggadget",
+        "sakai2gradebook"
+    ];
+
+    /**
+     * Customize the widgets available to be embedded on the dashboard
+     *
+     * For now, this is just a copy of the config in config.js
+     */
+    config.enabledDashboardWidgets = [
+        "recentmemberships",
+        "mycontent",
+        "activegroups",
+        "popularcontent",
+        "tags",
+        "jisccontent",
+        "mycontacts",
+        "accountpreferences",
+        "recentmessages",
+        "recentcontactsnew",
+        "mygroups",
+        "recentchangedcontent"
+    ];
+
+    /**
+     * Customize the widgets shown by default on a user's dashboard
+     * Note that this will only work for new users. Existing users will have
+     * to have their /~userid/private/privspace deleted to take changes
+     *
+     * For now, this is just a copy of the config in config.js
+     */
+    config.defaultprivstructure["${refid}5"].dashboard.columns =  {
+         "column1": [{
+             "uid": "${refid}10",
+             "visible": "block",
+             "name": "recentchangedcontent"
+         }],
+         "column2": [{
+             "uid": "${refid}11",
+             "visible": "block",
+             "name": "recentmemberships"
+         }],
+         "column3": [{
+             "uid": "${refid}12",
+             "visible": "block",
+             "name": "recentcontactsnew"
+         }]
+     };
+
+    /**
      * Kaltura Settings
      */
     config.kaltura = {
