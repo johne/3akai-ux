ad44663e6953683827c516b2099d33e5a6fc060f
Chris Roby <christopher.roby@gmail.com>
2012-02-04 12:21:48 -0800

    ACAD-661 Provide configuration example of how to change the default widget on a user's dashboard

--- dev/configuration/config_custom.js
+++ dev/configuration/config_custom.js
@@ -23,6 +23,39 @@ define(["config/config"], function(config) {
     config.displayLanguage = true;
 
     /**
+    * Change this variable to modify what widget is shown on each user's
+    * default dashboard page
+    */
+    var carouselWidget = 'carousel';
+
+    config.defaultprivstructure["${refid}0"] = {
+        "page": "<div class='fl-force-right'><button type='button' class='s3d-button s3d-margin-top-5 s3d-header-button s3d-header-smaller-button dashboard_change_layout' data-tuid='${refid}5'>__MSG__EDIT_LAYOUT__</button><button type='button' class='s3d-button s3d-margin-top-5 s3d-header-button s3d-header-smaller-button dashboard_global_add_widget' data-tuid='${refid}5'>__MSG__ADD_WIDGET__</button></div><div class='s3d-contentpage-title'>__MSG__MY_DASHBOARD__</div><div id='widget_" + carouselWidget + "' class='widget_inline'\></div><br/><div id='widget_dashboard_${refid}5' class='widget_inline'></div>"
+    };
+
+    /**
+     * Example of overriding the default widget on the top of the user's
+     * dashboard with the remotecontent widget
+     * The tricky part here is we need to set the data for the widget, which
+     * is why the widget is the id + a refid, and there is another refid
+     * line below to set the widget's data
+     */
+    // var carouselWidget = 'remotecontent_${refid}6';
+    // config.defaultprivstructure["${refid}0"] = {
+    //     "page": "<div class='fl-force-right'><button type='button' class='s3d-button s3d-margin-top-5 s3d-header-button s3d-header-smaller-button dashboard_change_layout' data-tuid='${refid}5'>__MSG__EDIT_LAYOUT__</button><button type='button' class='s3d-button s3d-margin-top-5 s3d-header-button s3d-header-smaller-button dashboard_global_add_widget' data-tuid='${refid}5'>__MSG__ADD_WIDGET__</button></div><div class='s3d-contentpage-title'>__MSG__MY_DASHBOARD__</div><div id='widget_" + carouselWidget + "' class='widget_inline'\></div><br/><div id='widget_dashboard_${refid}5' class='widget_inline'></div>"
+    // };
+    //
+    // config.defaultprivstructure["${refid}6"] = {
+    //     "remotecontent": {
+    //         "border_color": "ffffff",
+    //         "border_size": 0,
+    //         "height": "400",
+    //         "url": "http://rsmart.com",
+    //         "width": "100",
+    //         "width_unit": "%"
+    //     }
+    // };
+
+    /**
      * Kaltura Settings
      */
     config.kaltura = {
